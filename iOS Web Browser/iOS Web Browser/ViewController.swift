14578314c886feeba2306961c46331f7b5c123d0
Huzaifa Saboowala <huzaifa.saboowala96@gmail.com>
2021-03-29 22:42:38 -0700

    Implemented updating text field with current page URL. Implemented functionality for Back and Forward buttons

--- iOS Web Browser/iOS Web Browser/ViewController.swift
+++ iOS Web Browser/iOS Web Browser/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController,UITextFieldDelegate {
+class ViewController: UIViewController,UITextFieldDelegate,WKNavigationDelegate {
 
     @IBOutlet weak var backButton: UIButton!
     @IBOutlet weak var forwardButton: UIButton!
@@ -17,6 +17,9 @@ class ViewController: UIViewController,UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        urltextField.delegate = self
+        webView.navigationDelegate = self
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -45,5 +48,27 @@ class ViewController: UIViewController,UITextFieldDelegate {
         return true
     }
 
+    @IBAction func onBackButtonPressed(_ sender: Any) {
+     
+        if webView.canGoBack
+        {
+            webView.goBack()
+        }
+        
+    }
+    
+    @IBAction func onForwardButtonPressed(_ sender: Any) {
+        if webView.canGoForward
+        {
+            webView.goForward()
+        }
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        backButton.isEnabled = webView.canGoBack
+        forwardButton.isEnabled = webView.canGoForward
+        urltextField.text = webView.url?.absoluteString
+    }
+    
 }
 
