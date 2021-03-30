af8c858d558b402cc019ea5467576f2596ed5069
Huzaifa Saboowala <huzaifa.saboowala96@gmail.com>
2021-03-29 19:18:04 -0700

    Added Empty Project. Attached an empty webkit view and two buttons for Back and Forward to Main storyboard

---
+++ iOS Web Browser/iOS Web Browser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS Web Browser
+//
+//  Created by Huzaifa Saboowala on 3/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
