bb97d129d69dbaa5dc0cfbc6baa6521d62a6fd8d
Huzaifa Saboowala <huzaifa.saboowala96@gmail.com>
2021-03-29 22:07:15 -0700

    Added references to both buttons and webview in script. Created URL Object to create a default request to apple.com

--- iOS Web Browser/iOS Web Browser/ViewController.swift
+++ iOS Web Browser/iOS Web Browser/ViewController.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var forwardButton: UIButton!
+    @IBOutlet weak var webView: WKWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let url:URL = URL(string: "https://www.apple.com")!
+        let urlRequest:URLRequest = URLRequest(url: url)
+        webView.load(urlRequest)
+        
+    }
+    
+    
 
 }
 
